7246195a6785347d57e400f3d8f590f362568891
protogenesis <l_protogenesis@163.com>
2022-09-28 14:16:36 +0800

    feat: first commit

---
+++ lib/src/shared/app.dart
@@ -0,0 +1,60 @@
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'playback/bloc/bloc.dart';
+import 'providers/theme.dart';
+import 'router.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final settings = ValueNotifier(ThemeSettings(
+    sourceColor: Color.fromARGB(255, 175, 2, 255), // Replace this color
+    themeMode: ThemeMode.system,
+  ));
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<PlaybackBloc>(
+      create: (context) => PlaybackBloc(),
+      child: DynamicColorBuilder(
+        builder: (lightDynamic, darkDynamic) => ThemeProvider(
+            lightDynamic: lightDynamic,
+            darkDynamic: darkDynamic,
+            settings: settings,
+            child: NotificationListener<ThemeSettingChange>(
+              onNotification: (notification) {
+                settings.value = notification.settings;
+                return true;
+              },
+              child: ValueListenableBuilder<ThemeSettings>(
+                valueListenable: settings,
+                builder: (context, value, _) {
+                  final theme = ThemeProvider.of(context);
+                  // Create theme instance
+                  return MaterialApp.router(
+                    debugShowCheckedModeBanner: false,
+                    title: 'Flutter Demo',
+                    // Add theme
+                    // Add dark theme
+                    // Add theme mode
+                    theme: theme.light(settings.value.sourceColor),
+                    darkTheme: theme.dark(settings.value.sourceColor),
+                    themeMode: theme.themeMode(),
+                    routeInformationParser: appRouter.routeInformationParser,
+                    routeInformationProvider:
+                        appRouter.routeInformationProvider,
+                    routerDelegate: appRouter.routerDelegate,
+                  );
+                },
+              ),
+            )),
+      ),
+    );
+  }
+}
---
+++ lib/src/shared/views/adaptive_navigation.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveNavigation extends StatelessWidget {
+  const AdaptiveNavigation({
+    super.key,
+    required this.destinations,
+    required this.selectedIndex,
+    required this.onDestinationSelected,
+    required this.child,
+  });
+
+  final List<NavigationDestination> destinations;
+  final int selectedIndex;
+  final void Function(int index) onDestinationSelected;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, dimens) {
+        // Tablet Layout
+        if (dimens.maxWidth >= 600) {
+          // Add maxWidth constraint check
+          return Scaffold(
+            body: Row(
+              children: [
+                NavigationRail(
+                  extended: dimens.maxWidth >= 800,
+                  minExtendedWidth: 180,
+                  destinations: destinations
+                      .map((e) => NavigationRailDestination(
+                            icon: e.icon,
+                            label: Text(e.label),
+                          ))
+                      .toList(),
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: onDestinationSelected,
+                ),
+                Expanded(child: child),
+              ],
+            ),
+          );
+        }
+
+        return Scaffold(
+          body: child,
+          bottomNavigationBar: NavigationBar(
+            destinations: destinations,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onDestinationSelected,
+          ),
+        );
+        // Add closing curly bracket
+
+        // Add return for mobile layout
+      },
+    );
+  }
+}
